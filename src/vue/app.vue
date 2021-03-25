328addf4171ce2e4a81f2271e10314d3b76be0e3
Paul <paul@paul-mayer.eu>
2021-03-25 16:34:38 +0100

    init muliple framework tests

---
+++ src/vue/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="App">
+    <header class="App-header">
+     {{ message }}
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Learn Vue"
+    };
+  }
+};
+</script>
